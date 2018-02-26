c0818d69127949ab43f43f7181bd16ad21dade1b
Glauber Silva <glauber.silva@dressandgo.com.br>
2018-02-26 20:39:48 -0300

    PEP improvements

--- hackerrank/algorithms/diagonal_difference.py
+++ hackerrank/algorithms/diagonal_difference.py
@@ -4,12 +4,10 @@ import sys
 
 def diagonalDifference(a):
     # Complete this function
-    diag1, diag2, diff = 0,0,0
+    diag1, diag2, diff = 0, 0, 0
     for i in range(1, len(a)+1):
-        diag1+= a[i - 1][i - 1]
-        print("DIAG 1 : ", a[i - 1][i - 1])
-        diag2+= a[i-1][-i]
-        print("DIAG 2 : ", a[i-1][-i])
+        diag1 += a[i - 1][i - 1]
+        diag2 += a[i-1][-i]
 
     return abs(diag1-diag2)
 
@@ -17,7 +15,7 @@ if __name__ == "__main__":
     n = int(input().strip())
     a = []
     for a_i in range(n):
-       a_t = [int(a_temp) for a_temp in input().strip().split(' ')]
-       a.append(a_t)
+        a_t = [int(a_temp) for a_temp in input().strip().split(' ')]
+        a.append(a_t)
     result = diagonalDifference(a)
     print(result)
